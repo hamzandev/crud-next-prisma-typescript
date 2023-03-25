4ed9bc643b44757d655b4f48a9beda5190b9b3bd
Hamzan Dev <hamzankechil@gmail.com>
2023-03-26 06:28:47 +0800

    first

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div>
+      <div className="flex min-h-screen items-center-justify-center">
+        <h3 className="text-lg font-semibold text-primary">Loading...</h3>
+      </div>
+    </div>
+  );
+}
